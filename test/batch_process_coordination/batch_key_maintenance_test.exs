09c3c105b492b2d9ee14e929000f9b624d591bb7
Steve Wagner <ciroque@live.com>
2018-01-19 09:29:14 -0800

    Add tests for request_batch_key

--- test/batch_process_coordination/batch_key_maintenance_test.exs
+++ test/batch_process_coordination/batch_key_maintenance_test.exs
@@ -5,16 +5,69 @@ defmodule BatchProcessCoordination.BatchKeyMaintenanceTest do
 
   alias BatchProcessCoordination.{BatchKeyMaintenance, ProcessMaintenance}
 
-  @process_name "BPC-Test-Process"
+  @first_process_name "BPC-Test-Process-1"
+  @second_process_name "BPC-Test-Process-2"
+  @machine_one_name "BPC-Test-Machine-1"
 
   describe "BatchKeyMaintenance" do
     def setup do
       delete_all_process_batch_keys()
-      ProcessMaintenance.register_process(@process_name)
+    end
+
+    test "returns :no_keys_free when process is not registered" do
+      assert {:no_keys_free} = BatchKeyMaintenance.request_batch_key("NON-EXISTENT-PROCESS", "SOME MACHINE")
     end
 
     test "request_batch_key obtains a batch key" do
+      ProcessMaintenance.register_process(@first_process_name)
+      {:ok, %{key: key, machine: machine, process_name: process_name}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
 
+      assert key === 0
+      assert machine === @machine_one_name
+      assert process_name === @first_process_name
+    end
+
+    test "request_batch_key obtains successive batch keys" do
+      ProcessMaintenance.register_process(@first_process_name)
+      {:ok, %{key: key, machine: machine, process_name: process_name}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+
+      assert key === 0
+      assert machine === @machine_one_name
+      assert process_name === @first_process_name
+
+      {:ok, %{key: key, machine: machine, process_name: process_name}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+
+      assert key === 1
+      assert machine === @machine_one_name
+      assert process_name === @first_process_name
+
+      {:ok, %{key: key, machine: machine, process_name: process_name}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+
+      assert key === 2
+      assert machine === @machine_one_name
+      assert process_name === @first_process_name
+    end
+
+    test "deplete the key space results in :no_keys_free" do
+      ProcessMaintenance.register_process(@first_process_name)
+
+      for n <- 0..9 do
+        {:ok, %{key: key}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+        assert key === n
+      end
+
+      assert {:no_keys_free} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+      assert {:no_keys_free} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+    end
+
+    test "request_batch_key obtains a batch key when multiple processes exist" do
+      ProcessMaintenance.register_process(@first_process_name)
+      ProcessMaintenance.register_process(@second_process_name)
+      {:ok, %{key: key, machine: machine, process_name: process_name}} = BatchKeyMaintenance.request_batch_key(@first_process_name, @machine_one_name)
+
+      assert key === 0
+      assert machine === @machine_one_name
+      assert process_name === @first_process_name
     end
   end
 end
