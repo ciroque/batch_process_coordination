431ecce9622ca41f0c0308aaecc04104280df85a
Steve Wagner <ciroque@live.com>
2018-01-19 09:43:52 -0800

    Correct the type for request_batch_key return

--- lib/batch_process_coordination/batch_key_maintenance/batch_key_maintenance_behaviour.ex
+++ lib/batch_process_coordination/batch_key_maintenance/batch_key_maintenance_behaviour.ex
@@ -1,14 +1,21 @@
 defmodule BatchProcessCoordination.BatchKeyMaintenanceBehaviour do
   @type process_name_t :: String.t
   @type machine_name_t :: String.t
-  @type modulus_t :: integer()
+  @type key_t :: integer()
   @type last_completed_at_t :: String.t
+
+  @type batch_key_result_t :: %{
+    key: integer(),
+    machine: String.t,
+    process_name: String.t,
+    started_at: Timex.DateTime.t
+  }
 
   alias BatchProcessCoordination.ProcessBatchKeys
 
-  @callback request_batch_key(process_name_t, machine_name_t) :: {:ok, modulus_t} | {:error, map()}
+  @callback request_batch_key(process_name_t, machine_name_t) :: {:ok, batch_key_result_t} | {:error, map()} | {:error, String.t}
 
-  @callback release_batch_key(process_name_t, modulus_t) :: {:ok, last_completed_at_t} | {:error, map()}
+  @callback release_batch_key(process_name_t, key_t) :: {:ok, last_completed_at_t} | {:error, map()}
 
   @callback list_batch_keys(process_name_t) :: list(ProcessBatchKeys.t)
 end
